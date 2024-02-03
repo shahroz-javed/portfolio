2ce37597056fe2b04403cc0ab81c9d57b99ee6b1
shahroz-Javed <shahrozmaher198r@gmail.com>
2024-02-03 14:42:47 +0500

    base path netlify

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+console.log(window.origin);
+
 export default defineConfig({
   base:'portfolio',
   plugins: [vue()],
