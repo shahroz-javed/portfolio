d739f01b6c08a49c3139239fd99bb93401b1edd1
shahroz-Javed <shahrozmaher198r@gmail.com>
2024-02-03 14:58:52 +0500

    sdfsfd

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-console.log(window.origin);
 
 export default defineConfig({
   // base:'portfolio',
